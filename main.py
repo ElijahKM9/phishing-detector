a042a6c735c6f7e6eede922c6126d150bdda0af2
[Elijah McCalla] <elijahk.mccalla@gmail.com>
2026-04-21 15:33:24 -0400

    email testing added

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 from sendertest import emailTest
+
+score = 0
+sendAddress  = input("Enter their address:")
+
+
+score += emailTest(sendAddress)
+
+#subject  = input("Enter the email subject:")
+
+#body  = input("Enter the email body text:")
--- sendertest.py
+++ sendertest.py
@@ -1,5 +1,26 @@
 
+compAdd = "@gmail.com"
+def emailTest(sender):
+    score = 0
+    sender = sender.lower().strip()
+    if not sender.endswith(compAdd):
+            print("Not internal email address!")
+            score += 2
+    print("internal address!")
+    with open("readfile/whitelist.txt") as f:
+        for address in f:
+            address = address.strip()
+            if sender == address:
+                print("Address in whitelist!")
+                return score
+    with open("readfile/blacklist.txt") as f:
+        for address in f:
+            address = address.strip()
+            if sender == address:
+                print("Address in BlACKLIST!")
+                return score + 5
+        
+    print("External Not Pr-approved Email Address!")
+    return score
 
-def emailTest(sender, recepient):
-    open("phishing-detector\readfile\whitelist.txt" ,"rw")
-    return 0
+
